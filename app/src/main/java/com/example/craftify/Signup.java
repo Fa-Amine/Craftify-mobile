cfe3e1b2ef38a2eae27ef392292ef5887fdde96e
Fa-Amine <farah@gmail.com>
2024-12-25 20:49:31 +0100

    done with signup

--- app/src/main/java/com/example/craftify/Signup.java
+++ app/src/main/java/com/example/craftify/Signup.java
@@ -8,11 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private boolean isSignedIn = false;
-    private boolean isRegistered = false;
-
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
